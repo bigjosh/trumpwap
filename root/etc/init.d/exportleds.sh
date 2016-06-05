f51cf184b69afdbb83b85c78ba844190b1185552
bigjosh <github@junk.josh.com>
2016-06-05 11:58:58 -0400

    Make script +x

--- root/etc/init.d/exportleds.sh
+++ root/etc/init.d/exportleds.sh
@@ -1,5 +1,6 @@
 #export the leds so the squid redirectors can blink them
 #We only need to do this once per boot
+#this file is marked +x in git
 
 echo 17 >/sys/class/gpio/export
 echo 18 >/sys/class/gpio/export
